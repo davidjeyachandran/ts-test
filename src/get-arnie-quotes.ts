5d2409421f45586682d5cd30ee271ceaeaca9e54
David Jeyachandran <davidjeyachandran@gmail.com>
2022-03-10 00:11:13 +1100

    Implement getArnieQuotes

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,19 @@
 import { httpGet } from './mock-http-interface';
 
 // TODO define this type properly
-type TResult = any;
+type TResult = { [key: string]: string }
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
   // TODO: Implement this function.
-  
-  return [];
+
+  return await Promise.all(urls.map(async (item: string): Promise<TResult> => {
+    const result = await httpGet(item)
+
+    if (result.status === 200) {
+      return { 'Arnie Quote': JSON.parse(result.body).message }
+    } else {
+      return { 'FAILURE': JSON.parse(result.body).message }
+    }
+  }))
+
 };
