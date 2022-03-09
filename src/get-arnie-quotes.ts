0fa6a730dffa57dc14833984afb903a7ca34c6cd
David Jeyachandran <davidjeyachandran@gmail.com>
2022-03-10 00:37:58 +1100

    Simplify return code and add comment

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -2,16 +2,20 @@ import { httpGet } from './mock-http-interface';
 
 type TResult = { [key: string]: string }
 
+/**
+ * Executes a HTTP GET request on each of the URLs, transforms each of the
+ *     HTTP responses according to the challenge instructions and returns the results.
+ * @param {string[]} urls - The urls to be requested 
+ * @returns {Promise} - A promise which resolves to a results array
+ */
 export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
 
   return await Promise.all(urls.map(async (item: string): Promise<TResult> => {
     const result = await httpGet(item)
+    const msg = JSON.parse(result.body).message
 
-    if (result.status === 200) {
-      return { 'Arnie Quote': JSON.parse(result.body).message }
-    } else {
-      return { 'FAILURE': JSON.parse(result.body).message }
-    }
+    return (result.status === 200) ? { 'Arnie Quote': msg } : { 'FAILURE': msg }
+
   }))
 
 };
