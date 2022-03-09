72573f8f3c41103d76f7960727758c6cb76e4431
David Jeyachandran <davidjeyachandran@gmail.com>
2022-03-10 00:13:23 +1100

    Cleanup TODO messages

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,8 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
 type TResult = { [key: string]: string }
 
 export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
-  // TODO: Implement this function.
 
   return await Promise.all(urls.map(async (item: string): Promise<TResult> => {
     const result = await httpGet(item)
